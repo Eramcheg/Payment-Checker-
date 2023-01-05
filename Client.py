585cf2e8e559ffc36a92d5fc2be0271d56ca8157
Eramcheg <eramcheg@gmail.com>
2023-01-05 20:05:41 +0100

    Developed: Client Class

---
+++ Client.py
@@ -0,0 +1,5 @@
+class Client:
+    def __init__(self, Account_Number, Accountant_Account_Number):
+        self.rest=0
+        self.Account_Number = Account_Number
+        self.Accountant_Account_Number = Accountant_Account_Number
