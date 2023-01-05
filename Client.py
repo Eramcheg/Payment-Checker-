5f474af5a137137e27affad2d9c81fd1f36da82f
Eramcheg <eramcheg@gmail.com>
2023-01-05 21:44:26 +0100

    class Client, rest fixed

--- Client.py
+++ Client.py
@@ -1,5 +1,5 @@
 class Client:
-    def __init__(self, Account_Number, Accountant_Account_Number):
-        self.rest=0
+    def __init__(self, Account_Number,rest=0, Accountant_Account_Number=None):
+        self.rest=rest
         self.Account_Number = Account_Number
         self.Accountant_Account_Number = Accountant_Account_Number
